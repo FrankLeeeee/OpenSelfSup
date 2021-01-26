53c6e579aaed37aa17cc7ea811a57c15be5410ff
FrankLeeeee <somerlee.9@gmail.com>
2021-01-26 22:58:04 +0800

    fix import error

--- openselfsup/models/heads/__init__.py
+++ openselfsup/models/heads/__init__.py
@@ -1,6 +1,7 @@
 from .cls_head import ClsHead
 from .contrastive_head import ContrastiveHead
 from .contrastive_odc_head import ContrastiveODCHead
+from .contrastive_odc_head_v7 import ContrastiveODCHead_V7
 from .contrastive_odc_head_v15 import ContrastiveODCHead_V15
 from .contrastive_odc_head_v16 import ContrastiveODCHead_V16
 from .latent_pred_head import LatentPredictHead
