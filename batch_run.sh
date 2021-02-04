63f7addbdfd4416332ff47f48e04269c71dfb056
FrankLeeeee <somerlee.9@gmail.com>
2021-02-04 16:14:54 +0800

    added new models

--- batch_run.sh
+++ batch_run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-python tools/extract_backbone_weights.py ./work_dirs/fyp/100_cls_dataset/contrastive_odc_r50_v21_bs128/epoch_200.pth ./work_dirs/extracted_weights/contrastive_odc_r50_v21_bs128_epoch_200.pth
-python tools/extract_backbone_weights.py ./work_dirs/fyp/100_cls_dataset/contrastive_odc_r50_v22_bs128/epoch_200.pth ./work_dirs/extracted_weights/contrastive_odc_r50_v22_bs128_epoch_200.pth
 
-bash benchmarks/dist_train_linear.sh configs/benchmarks/fyp/imagenet_linear_classification/r50_last.py ./work_dirs/extracted_weights/contrastive_odc_r50_v21_bs128_epoch_200.pth
-bash benchmarks/dist_train_linear.sh configs/benchmarks/fyp/imagenet_linear_classification/r50_last.py ./work_dirs/extracted_weights/contrastive_odc_r50_v22_bs128_epoch_200.pth
+# bash ./benchmarks/dist_test_svm_epoch.sh ./configs/fyp/100_cls_dataset/contrastive_odc_r50_v20_ncluster10.py  200 "feat4 feat5" 1
+bash ./benchmarks/dist_test_svm_epoch.sh ./configs/fyp/100_cls_dataset/contrastive_odc_r50_v23.py  200 "feat4 feat5" 1
+
+# python tools/extract_backbone_weights.py ./work_dirs/fyp/100_cls_dataset/contrastive_odc_r50_v20_ncluster10/epoch_200.pth ./work_dirs/extracted_weights/contrastive_odc_r50_v20_ncluster10_epoch_200.pth
+python tools/extract_backbone_weights.py ./work_dirs/fyp/100_cls_dataset/contrastive_odc_r50_v23/epoch_200.pth ./work_dirs/extracted_weights/contrastive_odc_r50_v23_epoch_200.pth
+
+# bash benchmarks/dist_train_linear.sh configs/benchmarks/fyp/imagenet_linear_classification/r50_last.py ./work_dirs/extracted_weights/contrastive_odc_r50_v20_ncluster10_epoch_200.pth
+bash benchmarks/dist_train_linear.sh configs/benchmarks/fyp/imagenet_linear_classification/r50_last.py ./work_dirs/extracted_weights/contrastive_odc_r50_v23_epoch_200.pth
 
 
 
--- openselfsup/models/heads/__init__.py
+++ openselfsup/models/heads/__init__.py
@@ -5,5 +5,6 @@ from .contrastive_odc_head_v20 import ContrastiveODCHead_V20
 from .contrastive_odc_head_v21 import ContrastiveODCHead_V21
 from .contrastive_odc_head_v22 import ContrastiveODCHead_V22
 from .contrastive_odc_head_v23 import ContrastiveODCHead_V23
+from .contrastive_odc_head_v24 import ContrastiveODCHead_V24
 from .latent_pred_head import LatentPredictHead
 from .multi_cls_head import MultiClsHead
