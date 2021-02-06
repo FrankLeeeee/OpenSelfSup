7d0478e38704a61783283ac214d89b12e2c88710
FrankLeeeee <somerlee.9@gmail.com>
2021-02-06 16:26:02 +0800

    update

--- configs/fyp/100_cls_dataset/contrastive_odc_r50_v26_ncluster10_mincluster100.py
+++ configs/fyp/100_cls_dataset/contrastive_odc_r50_v26_ncluster10_mincluster100.py
@@ -4,8 +4,8 @@ _base_ = '../../base.py'
 
 # NOTE
 # set to 200? ODC has classes = 10000 while imagenet only has 1000 classes
-# num_classes = 20
-num_classes = 100
+num_classes = 20
+# num_classes = 100
 train_bs = 64
 
 model = dict(
@@ -37,8 +37,8 @@ model = dict(
         num_classes=num_classes),
     memory_bank=dict(
         type='ODCMemory',
-        # length=5052,
-        length=63916,
+        length=5052,
+        # length=63916,
         feat_dim=256,
         momentum=0.5,
         num_classes=num_classes,
@@ -49,8 +49,10 @@ data_source_cfg = dict(
     type='ImageNet',
     memcached=True,
     mclient_path='/mnt/lustre/share/memcached_client')
-data_train_list = 'data/imagenet/meta/subdataset/train_labeled_50percent_10interval_no_label.txt'
-# data_train_list = 'data/imagenet/meta/subdataset/train_labeled_20percent_50interval_no_label.txt'
+
+# data_train_list = 'data/imagenet/meta/subdataset/train_labeled_50percent_10interval_no_label.txt'
+data_train_list = 'data/imagenet/meta/subdataset/train_labeled_20percent_50interval_no_label.txt'
+
 data_train_root = 'data/imagenet/train'
 dataset_type = 'ContrastiveODCDataset'
 img_norm_cfg = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
@@ -159,7 +161,7 @@ checkpoint_config = dict(interval=20)
 total_epochs = 200
 
 log_config = dict(
-    interval=10,
+    interval=1,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='TensorboardLoggerHook')
