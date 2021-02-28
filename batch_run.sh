dbcb420f6f0046d5f0e0cb48ac1a9be5c60a4d22
FrankLeeeee <somerlee.9@gmail.com>
2021-02-28 20:24:16 +0800

    added new configs

--- batch_run.sh
+++ batch_run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes200_mincluster32.py 2
-bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes800_mincluster32.py 2
-bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes1600_mincluster32.py 2
+bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes400_mincluster32_abl_study_1.py 2
+bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes400_mincluster32_abl_study_2.py 2
+bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes400_mincluster32_abl_study_3.py 2
+bash tools/dist_train.sh configs/fyp/100_cls_dataset/contrastive_odc_r50_v24_bs128_lr2_classes400_mincluster32_abl_study_4.py 2
 
