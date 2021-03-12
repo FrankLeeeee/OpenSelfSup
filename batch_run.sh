09d6c2ee7380e660affd8b2ac7cb2006f90de469
FrankLeeeee <somerlee.9@gmail.com>
2021-03-12 22:42:45 +0800

    change batch run file

--- batch_run.sh
+++ batch_run.sh
@@ -8,3 +8,4 @@ bash tools/dist_train.sh configs/fyp/100_cls_dataset/moco_r50_v1_lr3.py 2
 bash tools/dist_train.sh configs/fyp/100_cls_dataset/moco_r50_v2_lr15.py 2
 bash tools/dist_train.sh configs/fyp/100_cls_dataset/moco_r50_v2_lr2.py 2
 bash tools/dist_train.sh configs/fyp/100_cls_dataset/moco_r50_v2_lr3.py 2
+
